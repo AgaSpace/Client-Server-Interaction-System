f848c4abfe8e65caa87713c4f761a5b0a3a4ca24
AkjaHAsLk1IALk0MasH <46046453+AgaSpace@users.noreply.github.com>
2021-04-07 18:15:24 +0700

    Fix 4
    Why do we need indexes when several of the same plugins can use the same indexes?

--- Client-Server-Interaction-System/DataBuilder.cs
+++ Client-Server-Interaction-System/DataBuilder.cs
@@ -9,45 +9,79 @@ namespace CSIS
 {
     public static class Controller
     {
-        public static NetPacket SendToClient(int indexData, int playerId, string data)
+        public static NetPacket SendToClient(string index, int playerId, string data)
         {
-            NetPacket packet = NetModule.CreatePacket<SCModule>(sizeof(int) + Encoding.UTF8.GetByteCount(data));
+            NetPacket packet = NetModule.CreatePacket<SCModule>(Encoding.UTF8.GetByteCount(index) + Encoding.UTF8.GetByteCount(data));
 
-            packet.Writer.Write(indexData);
+            packet.Writer.Write(index);
             packet.Writer.Write(data);
 
             NetManager.Instance.SendToClient(packet, playerId);
             return packet;
         }
 
-        public static NetPacket SendToClient(TSPlayer player, int indexData, string data)
+        #region one
+        public static NetPacket SendToClient(TSPlayer player, string indexData, string data)
         {
             return SendToClient(indexData, player.Index, data);
         }
 
-        public static NetPacket SendToClient(TSPlayer player, int indexData, Dictionary<string, object> data)
+        public static NetPacket SendToClient(TSPlayer player, string indexData, Dictionary<string, object> data)
         {
             return SendToClient(indexData, player.Index, JsonConvert.SerializeObject(data));
         }
 
-        public static NetPacket SendToClient(int player, int indexData, Dictionary<string, object> data)
+        public static NetPacket SendToClient(int player, string indexData, Dictionary<string, object> data)
         {
             return SendToClient(indexData, player, JsonConvert.SerializeObject(data));
         }
 
-        public static NetPacket SendToClient<T>(TSPlayer player, int indexData) where T : class
+        public static NetPacket SendToClient<T>(TSPlayer player, string indexData) where T : class
         {
             return SendToClient(indexData, player.Index, JsonConvert.SerializeObject(Activator.CreateInstance<T>()));
         }
 
-        public static NetPacket SendToClient<T>(int player, int indexData) where T : class
+        public static NetPacket SendToClient<T>(int player, string indexData) where T : class
         {
             return SendToClient(indexData, player, JsonConvert.SerializeObject(Activator.CreateInstance<T>()));
         }
 
-        public static NetPacket SendToClient(TSPlayer player, int indexData, object t)
+        public static NetPacket SendToClient(TSPlayer player, string indexData, object t)
         {
             return SendToClient(indexData, player.Index, JsonConvert.SerializeObject(t));
         }
+        #endregion
+
+        #region two
+        public static NetPacket SendToClient(string indexData, TSPlayer player, object t)
+        {
+            return SendToClient(indexData, player.Index, JsonConvert.SerializeObject(t));
+        }
+
+        public static NetPacket SendToClient(string indexData, TSPlayer player, string data)
+        {
+            return SendToClient(indexData, player.Index, data);
+        }
+
+        public static NetPacket SendToClient(string indexData, TSPlayer player, Dictionary<string, object> data)
+        {
+            return SendToClient(indexData, player.Index, JsonConvert.SerializeObject(data));
+        }
+
+        public static NetPacket SendToClient(string indexData, int player, Dictionary<string, object> data)
+        {
+            return SendToClient(indexData, player, JsonConvert.SerializeObject(data));
+        }
+
+        public static NetPacket SendToClient<T>(string indexData, TSPlayer player) where T : class
+        {
+            return SendToClient(indexData, player.Index, JsonConvert.SerializeObject(Activator.CreateInstance<T>()));
+        }
+
+        public static NetPacket SendToClient<T>(string indexData, int player) where T : class
+        {
+            return SendToClient(indexData, player, JsonConvert.SerializeObject(Activator.CreateInstance<T>()));
+        }
+        #endregion
     }
 }
