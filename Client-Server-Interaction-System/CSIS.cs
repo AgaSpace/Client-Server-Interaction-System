a765951d23ceb6416c155151c914dd5bc9913ebd
AkjaHAsLk1IALk0MasH <46046453+AgaSpace@users.noreply.github.com>
2021-04-12 21:44:08 +0700

    Can read

--- Client-Server-Interaction-System/CSIS.cs
+++ Client-Server-Interaction-System/CSIS.cs
@@ -11,18 +11,20 @@ using Terraria.Net;
 namespace CSIS
 {
     [ApiVersion(2, 1)]
-    public class MainPlugin : TerrariaPlugin
+    public class CSIS : TerrariaPlugin
     {
+        public static Dictionary<string, Action<string, int>> Reader = new Dictionary<string, Action<string, int>>();
+
         public override string Author => "Zoom L1";
         public override string Name => "CSIS";
 
-        public MainPlugin(Main main) : base(main) { }
+        public CSIS(Main main) : base(main) { }
 
         public override void Initialize() => ServerApi.Hooks.GamePostInitialize.Register(this, PostInitialize);
 
         public static void PostInitialize(EventArgs args)
         {
-            //NetManager.Instance._modules.Add(SCModule.Id, new SCModule());
+            NetManager.Instance._modules.Add(SCModule.Id, new SCModule());
         }
     }
 }
--- Client-Server-Interaction-System/ClientModule.cs
+++ Client-Server-Interaction-System/ClientModule.cs
@@ -13,6 +13,12 @@ namespace CSIS
 
         public override bool Deserialize(BinaryReader reader, int userId)
         {
+            string key = reader.ReadString();
+            string json = reader.ReadString();
+
+            if (CSIS.Reader.ContainsKey(key))
+                CSIS.Reader[key].Invoke(json, userId);
+
             return false;
         }
     }
